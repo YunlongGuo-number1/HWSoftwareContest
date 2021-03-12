83e7fbc273299d5d66ab85941fca8b4c33bbed48
Crystal-Dragon-Liu <LybCrystal@163.com>
2021-03-12 17:09:58 +0800

    update class Server and VirtualMachine

--- SDK/SDK_python/CodeCraft-2021/src/config.py
+++ SDK/SDK_python/CodeCraft-2021/src/config.py
@@ -15,6 +15,9 @@ cpu_core = int
 MEMORY = 'memory_capacity'
 CORE = 'cpu_core'
 specification_dict = dict
+model_type = str
+INVALID_MODEL = 'NULl'
+
 
 
 # these types below are about to define the cost of a server.
@@ -26,3 +29,32 @@ cost_dict = dict
 
 # serial number of servers.
 server_number = int
+SERVER_NUMBER = "server_number"
+NODE_NAME = "node_name"
+
+# node and vector
+node_name = str
+A = 'A'
+B = 'B'
+INVALID_NODE = 'NULL'
+INVALID_SERVER_NUM = -1
+
+vector_size = int
+
+
+# virual machine users purchase
+vm_type = str
+INVALID_POSITION = {SERVER_NUMBER: INVALID_SERVER_NUM,
+                    NODE_NAME: INVALID_NODE}
+server_number_index = 0
+node_index = 1
+
+
+ID = int
+INVALID_ID = -1
+
+
+# check capacity 
+node_mode = bool
+double_mode = True
+single_mode = False
