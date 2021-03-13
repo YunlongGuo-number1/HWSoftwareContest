7df4203e65ec9f2b4ffc05022f8c56ab0971f4c7
云子谣 <crystal-dragon-lyb@yunziyaodeMacBook-Pro.local>
2021-03-13 18:06:26 +0800

    some update in terms of server.py

--- SDK/SDK_python/CodeCraft-2021/src/VirtualMachine.py
+++ SDK/SDK_python/CodeCraft-2021/src/VirtualMachine.py
@@ -24,31 +24,42 @@ class VirtualMachine():
         # TODO maybe there are some logical problems.
         self._current_position = config.INVALID_POSITION
         self._node_mode = node_mode
+
     # 获得所有CPU核心数
     def get_all_cpu_core(self)->config.cpu_core:
         return self._spec[config.CORE]
+
     # 获得所有内存
     def get_all_memory(self)->config.memory_capacity:
         return self._spec[config.MEMORY]
+
     # 获取该虚拟机对象的规格，包含核心数和所需内存
     def get_spec(self)->config.specification_dict:
         return self._spec
+
     # 获取节点模式，如双节点或单节点
     def get_node_mode(self)->config.node_mode:
         return self._node_mode
+
     # 获取虚拟机对象的id
     def get_id(self)->config.ID:
         return self._id
+
     # 根据节点模式，返回对应的CPU核心数需求
     def get_cpu_required(self)->config.cpu_core:
         cpu_required = self._spec[config.CORE] if self._node_mode == config.node_mode.double_mode else self._spec[config.CORE] / 2
         return cpu_required
+
     # 根据节点模式，返回对应的内存需求
     def get_mem_required(self)->config.memory_capacity:
         mem_required = self._spec[config.MEMORY] if self._node_mode == config.node_mode.double_mode else self._spec[config.MEMORY] / 2
         return mem_required
+
     # 在一个虚拟机实例被部署到服务器节点上时，应当设置虚拟机实例的当前部署位置
     # 在迁移时，也应当调用该接口来重置虚拟机部署位置
     def set_location(self, server_num: config.server_number, node: config.node_name):
         self._current_position = {config.SERVER_NUMBER: server_num,
                                   config.NODE_NAME: node}
+                                  
+    def delete_location(self):
+        self._current_position = config.INVALID_POSITION
